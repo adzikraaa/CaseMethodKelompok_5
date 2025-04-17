acf9d4b1bd7aa88626daea3a0e50b41d57faffed
anjalivio <anjaliviolita39@gmail.com>
2025-04-17 15:14:21 +0700

    class penilaian

--- penilaian.java
+++ penilaian.java
@@ -1,17 +1,22 @@
 public class penilaian {
-    String mahasiswa;
-    String matakuliah;
-    double nilaiTugas, nilaiUTS, nilaiUAS, nilaiAkhir;
+    mahasiswa mahasiswa;
+    matakuliah mataKuliah;
+    double tugas, uts, uas;
 
-    public penilaian(String mahasiswa, String matakuliah, double nilaiTugas, double nilaiUTS, double nilaiUAS,
-            double nilaiAkhir) {
+    public penilaian (mahasiswa mahasiswa, matakuliah mataKuliah, double tugas, double uts, double uas) {
         this.mahasiswa = mahasiswa;
-        this.matakuliah = matakuliah;
-        this.nilaiAkhir = nilaiAkhir;
-        this.nilaiTugas = nilaiTugas;
-        this.nilaiUAS = nilaiUAS;
+        this.mataKuliah = mataKuliah;
+        this.tugas = tugas;
+        this.uts = uts;
+        this.uas = uas;
     }
-public void hitungNilaiAkhir(){
-    System.out.println();
-}
+
+    public double hitungNilaiAkhir() {
+        return (tugas * 0.3) + (uts * 0.3) + (uas * 0.4);
+    }
+
+    public void tampil() {
+        System.out.printf("NIM: %s, Nama: %s, MK: %s, Tugas: %.1f, UTS: %.1f, UAS: %.1f, NA: %.2f\n",
+                mahasiswa.nim, mahasiswa.nama, mataKuliah.namaMK, tugas, uts, uas, hitungNilaiAkhir());
+    }
 }
