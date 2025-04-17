a117ee752716f5124ae152cdbe4853b3cbdf6c12
adzikraaa <dzikra.zen@gmail.com>
2025-04-17 14:07:16 +0700

    class mahasiswa

---
+++ mahasiswa.java
@@ -0,0 +1,11 @@
+public class mahasiswa {
+    String = nim;
+    String = nama;
+    String = prodi; 
+}
+
+    public mahasiswa (String nm, String nama, String prodi){
+this.nim = nim;
+this.nama = nam;
+this.prodi = prodi;
+}
