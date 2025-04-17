ad2180a0c574f05248897a17f0653a8b25afb3ad
adzikraaa <dzikra.zen@gmail.com>
2025-04-17 17:40:14 +0700

    update penilaian

--- penilaian.java
+++ penilaian.java
@@ -1,5 +1,4 @@
 public class penilaian {
-<<<<<<< HEAD
     mahasiswa Mahasiswa;
     matakuliah MataKuliah;
     double tugas, uts, uas;
@@ -7,15 +6,6 @@ public class penilaian {
     public penilaian (mahasiswa mahasiswa, matakuliah mataKuliah, double tugas, double uts, double uas) {
         this.Mahasiswa = mahasiswa;
         this.MataKuliah = mataKuliah;
-=======
-    mahasiswa mahasiswa;
-    matakuliah mataKuliah;
-    double tugas, uts, uas;
-
-    public penilaian (mahasiswa mahasiswa, matakuliah mataKuliah, double tugas, double uts, double uas) {
-        this.mahasiswa = mahasiswa;
-        this.mataKuliah = mataKuliah;
->>>>>>> 5657ce8b0ec59ba506e310e3d7d6586a0f648a15
         this.tugas = tugas;
         this.uts = uts;
         this.uas = uas;
@@ -26,15 +16,9 @@ public class penilaian {
     }
 
     public void tampil() {
-<<<<<<< HEAD
         System.out.println("NIM: " + Mahasiswa.nim +
                            ", Nama: " + Mahasiswa.nama +
                            ", MK: " + MataKuliah.namaMK +
-=======
-        System.out.println("NIM: " + mahasiswa.nim +
-                           ", Nama: " + mahasiswa.nama +
-                           ", MK: " + mataKuliah.namaMK +
->>>>>>> 5657ce8b0ec59ba506e310e3d7d6586a0f648a15
                            ", Tugas: " + tugas +
                            ", UTS: " + uts +
                            ", UAS: " + uas +
