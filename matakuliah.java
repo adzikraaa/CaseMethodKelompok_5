7d56deb6f6bdcc61ec7eba190115e7845df69876
sukmaannda <p.sukmaananda@gmail.com>
2025-04-17 14:23:41 +0700

    class matakuliah

---
+++ matakuliah.java
@@ -0,0 +1,12 @@
+public class matakuliah {
+    String kodeMK;
+    String namaMK;
+    int sks; 
+
+
+public matakuliah (String kodeMK, String namaMk, int sks){
+    this.kodeMK = kodeMK;
+    this.namaMK = namaMK;
+    this.sks = sks;
+    }
+}
