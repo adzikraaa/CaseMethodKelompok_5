286eadfd75180481fbb6abb5a59e8c399e779c29
adzikraaa <dzikra.zen@gmail.com>
2025-04-17 18:49:07 +0700

    update mata kuliah

--- matakuliah.java
+++ matakuliah.java
@@ -4,7 +4,7 @@ public class matakuliah {
     int sks; 
 
 
-public matakuliah (String kodeMK, String namaMk, int sks){
+public matakuliah (String kodeMK, String namaMK, int sks){
     this.kodeMK = kodeMK;
     this.namaMK = namaMK;
     this.sks = sks;
