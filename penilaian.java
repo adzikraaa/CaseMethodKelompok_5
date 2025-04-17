5657ce8b0ec59ba506e310e3d7d6586a0f648a15
anjalivio <anjaliviolita39@gmail.com>
2025-04-17 15:19:46 +0700

    class penilaian

--- penilaian.java
+++ penilaian.java
@@ -16,7 +16,12 @@ public class penilaian {
     }
 
     public void tampil() {
-        System.out.printf("NIM: %s, Nama: %s, MK: %s, Tugas: %.1f, UTS: %.1f, UAS: %.1f, NA: %.2f\n",
-                mahasiswa.nim, mahasiswa.nama, mataKuliah.namaMK, tugas, uts, uas, hitungNilaiAkhir());
+        System.out.println("NIM: " + mahasiswa.nim +
+                           ", Nama: " + mahasiswa.nama +
+                           ", MK: " + mataKuliah.namaMK +
+                           ", Tugas: " + tugas +
+                           ", UTS: " + uts +
+                           ", UAS: " + uas +
+                           ", NA: " + hitungNilaiAkhir());
     }
 }
