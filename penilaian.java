acf4d801c0c39f0c81d10fc3a826a989b717e844
anjalivio <anjaliviolita39@gmail.com>
2025-04-17 14:48:47 +0700

    class penilaian

---
+++ penilaian.java
@@ -0,0 +1,17 @@
+public class penilaian {
+    String mahasiswa;
+    String matakuliah;
+    double nilaiTugas, nilaiUTS, nilaiUAS, nilaiAkhir;
+
+    public penilaian(String mahasiswa, String matakuliah, double nilaiTugas, double nilaiUTS, double nilaiUAS,
+            double nilaiAkhir) {
+        this.mahasiswa = mahasiswa;
+        this.matakuliah = matakuliah;
+        this.nilaiAkhir = nilaiAkhir;
+        this.nilaiTugas = nilaiTugas;
+        this.nilaiUAS = nilaiUAS;
+    }
+public void hitungNilaiAkhir(){
+    System.out.println();
+}
+}
