33831f6dd0632776f7263734b19a249f00be276d
adzikraaa <dzikra.zen@gmail.com>
2025-04-17 14:13:44 +0700

    class mahasiswa

--- mahasiswa.java
+++ mahasiswa.java
@@ -1,11 +1,14 @@
 public class mahasiswa {
-    String = nim;
-    String = nama;
-    String = prodi; 
+    String nim, nama, prodi;
+
+    public mahasiswa(String nim, String nama, String prodi) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+
+    public void tampil() {
+        System.out.println("NIM: " + nim + ", Nama: " + nama + "Prodi"+ prodi);
+    }
 }
 
-    public mahasiswa (String nm, String nama, String prodi){
-this.nim = nim;
-this.nama = nam;
-this.prodi = prodi;
-}
